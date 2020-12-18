69942dbce5ab4f7443d9999ee8e9c60ad4e98c29
AbusedLogic <burkebrennon123@gmail.com>
2020-12-17 19:55:25 -0500

    lots of information

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//required
+const con = require("./connector").con
 //Tools
 const express = require('express');
 const app = express();
@@ -10,8 +12,60 @@ app.use(bodyParser.json());
 app.get("/",(req,res)=>{
     res.render("index",{title:'Home Page'});
 })
+/*
 app.post("/database/Search",(req,res)=>{
     res.send(req.body.searchbar)
+    con.query(`SELECT * FROM hondacolors WHERE MAKE = '${req.body.searchbar}';`,(err,res)=>{
+        if(err){
+            console.log(err)
+        }
+        console.log(res)
+    })
+})
+
+app.post("/database/Search",(req,res)=>{
+    res.send(req.body.searchbar)
+    let a = "SELECT * FROM hondacolors"
+    if(req.body.searchbar != ''){
+        a += ` WHERE COLORCODE = '${req.body.searchbar}'`
+    }
+    a+=";"
+    con.query(a,(err,res)=>{
+        if(err){
+            console.log(err)
+        }
+        console.log(res)
+    })
+})
+*/
+app.post("/database/search",(req,res)=>{
+    //res.send(req.body.searchbar)
+    let a = "SELECT * FROM hondacolors"
+    if(req.body.searchbar != ''){
+        a += ` WHERE COLORCODE = '${req.body.searchbar}'`
+    }
+    a+=";"
+    con.query(a,(err,result)=>{
+        if(err){
+            console.log(err);
+            res.send(err);
+        }
+        else{
+            let arr = [];
+            for(let i in result){
+                arr[i] = [];
+                for(let obj in result[i]){
+                    arr[i].push(`${obj}:${result[i][obj]}`)
+                }
+            }
+            res.render("results",{
+                list:arr
+            })
+        }
+    })
+})
+app.get("*",(req,res)=>{
+    res.redirect("/")
 })
 app.listen(5001,()=>{
     console.log("its running")
